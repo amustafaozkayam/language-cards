1c7e7c4259f13b431b6b41f20af7f08531337c4e
Mustafa <amustafaozkayam@gmail.com>
2022-03-28 18:17:28 +0300

    setTimeout added

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,19 @@ import React, { useState } from "react";
 
 export default function Card({ name, img, options }) {
   const [isVisible, setIsvisible] = useState(true);
+  // const [isClik, setClik] =useState(true)
   return (
     <>
-      <div key={name} className="card" onClick={() => setIsvisible(!isVisible)}>
+      <div
+        key={name}
+        className="card"
+        onClick={() => {
+          setIsvisible(!isVisible);
+          setTimeout(() => {
+            setIsvisible(isVisible);
+          }, 3000);
+        }}
+      >
         {isVisible ? (
           <div>
             <img src={img} alt="" />
